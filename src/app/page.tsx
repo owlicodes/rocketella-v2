9c6cc396e4bd7b30cf947e904e4db44723dffd91
Jose Diago <owlicodes@gmail.com>
2024-11-05 09:21:43 +0800

    Header and hero section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { Header } from "@/features/common/header";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="relative">
+      <Header />
+
+      <section className="flex min-h-screen items-center bg-primary text-white">
+        <div className="container mx-auto px-4 py-32 text-center md:py-64">
+          <h1 className="mb-4 text-6xl font-bold md:text-7xl">
+            we make things happen
+          </h1>
+          <p className="mx-auto mb-8 max-w-2xl text-lg">
+            We produce top notch bespoke solutions for business, big and small
+          </p>
+          <Button className="rounded-none bg-[#F58B1F] px-6 py-6">
+            FIND YOUR SOLUTIONS
+          </Button>
+        </div>
+      </section>
+    </div>
+  );
 }
---
+++ src/features/common/header.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+import { Button } from "@/components/ui/button";
+
+export const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header
+      className={`w-full transition-all duration-300 ${
+        isScrolled
+          ? "fixed top-0 bg-black text-white shadow-lg"
+          : "absolute bg-transparent"
+      }`}
+    >
+      <div className="container mx-auto flex items-center justify-between px-4 py-4">
+        <div className="text-2xl font-bold">
+          <Image
+            src="/brand-logo.png"
+            alt="Rocketella Logo"
+            width="130"
+            height="130"
+            className="h-auto w-auto"
+          />
+        </div>
+        <div className="flex items-center space-x-4">
+          <nav className="hidden space-x-4 text-white md:flex">
+            <Link href="/">HOME</Link>
+            <Link href="/">ABOUT</Link>
+            <Link href="/">SERVICE</Link>
+            <Link href="/">MENU</Link>
+            <Link href="/">PAGES</Link>
+            <Link href="/">CONTACT</Link>
+          </nav>
+          <Button variant="outline" className="text-primary">
+            GET IN TOUCH
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
